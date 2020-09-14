cad22f1c5ee8236d2f0c2ddea838b7942d88543e
Kuzm1ch-dev <steam.danya@yandex.ru>
2020-09-15 01:32:42 +0300

    crosshair and fire effect

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public Vector2 size;
+    public Texture2D crosshair;
+   void OnGUI()
+    {
+        
+        GUI.DrawTexture(new Rect(Screen.width/2 - size.x/2,Screen.height/2 - size.y/2, size.x, size.y),crosshair);
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
