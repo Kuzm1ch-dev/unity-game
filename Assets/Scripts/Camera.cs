971adf94211505ff1f5658a73d2e96b6b578b6bb
Kuzm1ch-dev <steam.danya@yandex.ru>
2020-09-14 20:31:18 +0300

    init

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Vector2 mouseAxis;
+    public Transform Player;
+
+    float xRotation;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseAxis.x = Input.GetAxis("Mouse X");
+        mouseAxis.y = Input.GetAxis("Mouse Y");
+
+        xRotation -= mouseAxis.y;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        Player.Rotate(Vector3.up * mouseAxis.x);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator charterAnimator;
+    public CharacterController characterController;
+
+    public float gravity = 9.81f;
+    public float speed = 5;
+    public float jumpStrength = 3.5f;
+
+    float directionY;
+
+    public Vector2 Axis;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Axis.x = Input.GetAxis("Horizontal");
+        Axis.y = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(Axis.x,0,Axis.y);
+
+        if (characterController.isGrounded)
+        {
+            charterAnimator.SetFloat("FallTime", 0);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                directionY = jumpStrength;
+                charterAnimator.SetTrigger("Jump");
+            }
+
+            if (Axis != Vector2.zero)
+            {
+                charterAnimator.SetFloat("WalkFloat", Axis.y);
+                charterAnimator.SetBool("Walk", true);
+            }
+            else
+            {
+                charterAnimator.SetBool("Walk", false);
+            }
+
+        }
+        else
+        {
+            charterAnimator.SetFloat("FallTime",charterAnimator.GetFloat("FallTime") + Time.deltaTime);
+        }
+
+        directionY -= gravity * Time.deltaTime;
+        dir.y = directionY;
+
+        characterController.Move(transform.TransformDirection(dir) * speed * Time.deltaTime);
+
+
+    }
+}
