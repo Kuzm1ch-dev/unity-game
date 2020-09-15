c8aa1dcce70d1e719b8ff454f5987a3ef4bb8448
Kuzm1ch-dev <steam.danya@yandex.ru>
2020-09-15 08:56:18 +0300

    other

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -10,11 +10,12 @@ public class WeaponScript : MonoBehaviour
     public Transform FirePoint;
     public ParticleSystem muzzleEffect;
 
+    public GameObject decal;
+
     public float delay;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -31,6 +32,16 @@ public class WeaponScript : MonoBehaviour
         yield return new WaitForSeconds(delay);
         audioSource.PlayOneShot(sound);
         muzzleEffect.Play();
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Quaternion hitRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            GameObject Decal = Instantiate<GameObject>(decal, hit.point + (hit.normal * 0.001f),hitRotation);
+            decal.transform.parent = hit.collider.transform;
+        }
+
         StopCoroutine("Fire");
     }
 }
